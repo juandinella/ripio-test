5b351eee12d1a60bf2f4123bec19a717be3be7b4
Juan Di Nella <juan.dinella@gmail.com>
2021-01-22 17:31:34 -0300

    create style structure

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/styles/components/Sidebar.scss'
+
+const Sidebar = () => (
+  <aside className='sidebar'>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim ex saepe nobis aut, blanditiis quod vero perspiciatis dolorem! Officiis alias dolore corrupti libero quis iusto possimus at, distinctio soluta eos?</p>
+  </aside>
+);
+
+export default Sidebar
