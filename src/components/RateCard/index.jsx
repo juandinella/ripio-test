adf33150d7765811136e32f8ac85025cc58015bd
Juan Di Nella <juan.dinella@gmail.com>
2021-01-23 19:38:12 -0300

    improve api

--- src/components/RateCard/index.jsx
+++ src/components/RateCard/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './index.scss';
 
-const RateCard = ({ buy_rate }) => (
-  <div className="rateCard">
-    <div className='rateCard-amount'>{buy_rate}</div>
+const RateCard = ({ ticker, buy_rate }) => (
+  <div className="rate-card flex flex-column justify-between">
+    <div>
+      <div className='rate-card_ticker'>
+        <span>{ticker}</span>
+      </div>
+      <div className='rate-card_amount'>$ {buy_rate}</div>
+    </div>
+    <div className='rate-card_variation'>Últimas 24hs</div>
   </div>
 );
 
