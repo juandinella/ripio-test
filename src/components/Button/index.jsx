ef95ecbaedeeaf9d1bb1d1498e5394e13501ed1a
Juan Di Nella <juan.dinella@gmail.com>
2021-01-24 11:07:36 -0300

    fix button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import './Index.scss';
+import './index.scss';
 import classNames from "classnames"
 
 const Button = ({ link, variant, size, children }) => (
   <a href={link} className={classNames('btn', variant, size)}>{children}</a>
 );
-
 export default Button
